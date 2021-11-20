24427e0531e39ca93b84eaed832c2e9dde6a4584
MarkAppprogrammer <80441909+MarkAppprogrammer@users.noreply.github.com>
2021-11-20 15:05:16 -0800

    Updated netscan.py

--- netscan.py
+++ netscan.py
@@ -53,26 +53,14 @@ print("")
 IP = input("[*]Enter IP: ")
 IP = dst_ip
 sleep(0.5)
-print("[*] Using nmap to scan for vunls...")
-sleep(0.5)
-print("[*] This may take some time...")
 
-
-#runs nmap command
-command = "nmap -sC -sV " + IP #EDIT NMAP COMMAND
-sys.stdout.flush()
-p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-(output, err) = p.communicate()
-newoutput = str(output, 'UTF-8')
-
-#prints output of nmap
-print ("{}".format(newoutput))
-
-#chek interface 
+#check interface 
 print("[*] What interface are you using?")
 interface = input("[*] Interface: ")
 print("[*] Checking port vulns using scapy...")
 sleep(0.5)
+homescreen()
+
 
 #all functions
 def webservertests():
@@ -87,24 +75,82 @@ def webservertests():
 	(output, err) = p.communicate()
 	newoutput = str(output, 'UTF-8')
 
-#sending packets
-tcp_connect_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=10)
-if(tcp_connect_scan_resp.haslayer(TCP)):
-    if(tcp_connect_scan_resp.getlayer(TCP).flags == 0x12):
-        send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="AR"),timeout=10)
-        print("[*] Port 80 is Open")
-        print("[*] Possibly a web server running on " + IP)
-    elif (tcp_connect_scan_resp.getlayer(TCP).flags == 0x14):
-        print ("[*] Port 80 is closed, trying port 443")
-        dst_port = 443
-        tcp_connect_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=10)
-			if(tcp_connect_scan_resp.haslayer(TCP)):
-				if(tcp_connect_scan_resp.getlayer(TCP).flags == 0x12):
-					send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="AR"),timeout=10)
-					print("[*] Port 443 is Open")
-					sleep(0.5)
-					print("[*] Possibly a web server running on " + IP)
-					webservertests()
-				elif (tcp_connect_scan_resp.getlayer(TCP).flags == 0x14):
-					print ("[*] Both Port 80 and Pory 443 are closed on " + IP)
+def nmapscan()
+	print("[*] Using nmap to scan for vunls...")
+	sleep(0.5)
+	print("[*] This may take some time...")
 
+	#runs nmap command
+	command = "nmap -sC -sV " + IP #EDIT NMAP COMMAND
+	sys.stdout.flush()
+	p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+	(output, err) = p.communicate()
+	newoutput = str(output, 'UTF-8')
+
+	#prints output of nmap
+	print ("{}".format(newoutput))
+	homescreen()
+
+def basicostests():
+	sleep(0.5)
+	print("[*] Not redy yet, Check agian later")
+	homescreen()
+	
+def basicnettests():
+	sleep(0.5)
+	print("[*] Not redy yet, Check agian later")
+	homescreen()
+
+def basicwebtests()
+	#sending packets
+	tcp_connect_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=10)
+	if(tcp_connect_scan_resp.haslayer(TCP)):
+		if(tcp_connect_scan_resp.getlayer(TCP).flags == 0x12):
+			send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="AR"),timeout=10)
+			print("[*] Port 80 is Open")
+			sleep(0.5)
+			print("[*] Possibly a web server running on " + IP)
+			webservertests()
+		elif (tcp_connect_scan_resp.getlayer(TCP).flags == 0x14):
+			print ("[*] Port 80 is closed, trying port 443")
+			dst_port = 443
+			tcp_connect_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=10)
+				if(tcp_connect_scan_resp.haslayer(TCP)):
+					if(tcp_connect_scan_resp.getlayer(TCP).flags == 0x12):
+						send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="AR"),timeout=10)
+						print("[*] Port 443 is Open")
+						sleep(0.5)
+						print("[*] Possibly a web server running on " + IP)
+						webservertests()
+					elif (tcp_connect_scan_resp.getlayer(TCP).flags == 0x14):
+						print ("[*] Both Port 80 and Pory 443 are closed on " + IP)
+						sleep(0.5) 
+						homescreen()
+
+
+def homescreen():
+	print("[*] What tests would you like to do: ")
+	print("1-> webtests")
+	print("2-> osrelatedtests")
+	print("3-> networktests")
+	print("4-> run a nmap scan")
+	testtype = input("[*] Tests 1 or 2 or 3 or 4: ")
+	if (testtype == "1"):
+		basicwebtests()
+	elif (testtype == "2"):
+		basicostests()
+	elif (testtype == "3"):
+		basicnettests()
+	elif (testtype == "4")
+		nmapscan()
+	else:
+		print("[*] Please Enter 1,2,3, or 4: ")
+		homescreen()
+	
+	
+	
+	
+	
+	
+	
+		
